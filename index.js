906e8213d2d2d44af0bee2ede23117a363be180d
AleksaCorona <aleksaprebiracevicorona@outlook.com>
2022-08-07 12:34:49 +0200

    Add a function to generate an unique password

---
+++ index.js
@@ -0,0 +1,21 @@
+const letters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "1234567890";
+const symbols = "!@#$%^&*()	";
+const characters = [letters.toUpperCase(), letters, numbers, symbols].join("");
+
+/**
+ * Generate unique, quick and secure passwords.
+ *
+ * @param {number} length
+ */
+const generatePassword = (length) => {
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+};
+
+module.exports = generatePassword;
