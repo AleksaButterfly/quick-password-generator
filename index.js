fb5393d118eb74adf5ea5b7e673c8e42771effa6
AleksaCorona <aleksaprebiracevicorona@outlook.com>
2022-08-07 13:14:15 +0200

    Remove empty string symbols

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const letters = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "1234567890";
-const symbols = "!@#$%^&*()	";
+const symbols = "!@#$%^&*()";
 const characters = [letters.toUpperCase(), letters, numbers, symbols].join("");
 
 /**
